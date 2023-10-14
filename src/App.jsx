f7fafeed2089b3c4515f5bb835d0b40bfd7add76
lawrenceuchenye <lawuche29@gmail.com>
2023-10-14 10:55:22 +0100

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import { BrowserRouter, Routes } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+
+import Home from "./Pages/Home";
 
 function App() {
   return (
     <div className="main-container">
-      <h1>React Router 6</h1>
+      <h1 className="title-container">React Router 6</h1>
       <BrowserRouter>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/contact"
+            element={
+              <div>
+                <h1>CONTACT PAGE</h1>
+              </div>
+            }
+          />
+          <Route
+            path="/about"
+            element={
+              <div>
+                <h1>ABOUT PAGE</h1>
+              </div>
+            }
+          />
+        </Routes>
       </BrowserRouter>
     </div>
   );
---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const index = () => {
+  return (
+    <div className="container">
+      <h1>HomePage</h1>
+      <div className="btn-container">
+        <button>Contact</button>
+        <button>About </button>
+      </div>
+    </div>
+  );
+};
+
+export default index;
