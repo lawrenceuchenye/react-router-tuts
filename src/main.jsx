342a3902797a1118c06c44e0d815d4f2cf7d9f1c
lawrenceuchenye <lawuche29@gmail.com>
2023-10-14 09:07:00 +0100

    we moving

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <div>
+    <div>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&family=Yuji+Hentaigana+Akari&display=swap"
+        rel="stylesheet"
+      />
+    </div>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </div>
+);
